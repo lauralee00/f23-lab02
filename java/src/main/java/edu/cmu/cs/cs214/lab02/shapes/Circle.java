5d1f15564b46d125799623453e31bc092deff2ae
incompetentcoderoops <laura.yeheun.lee@gmail.com>
2023-09-06 15:13:35 -0400

    Add Shape Encapsulation

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Circle.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Circle.java
@@ -1,13 +1,13 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Circle implements Shape {
-    public double radius;
+    private double radius;
 
     public Circle(double radius) {
         this.radius = radius;
     }
 
     public double getArea() {
-        return Math.PI * radius * radius;
+        return Math.PI * this.radius * this.radius;
     }
 }
--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Square.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Square.java
@@ -1,13 +1,13 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Square implements Shape {
-    public double sideLen;
+    private double sideLen;
 
     public Square(double sideLen) {
         this.sideLen = sideLen;
     }
     
     public double getArea() {
-        return sideLen * sideLen;
+        return this.sideLen * this.sideLen;
     }
 }
